1b818cdc35e3fb3f334af3ba3d6ba1a824ceca9a
Ayca96 <aycakubra1@gmail.com>
2024-07-06 14:51:59 +0200

    "Added empty lines in App.js"

--- patisserie/src/App.js
+++ patisserie/src/App.js
@@ -6,6 +6,7 @@ import MyCarousel from "./components/MyCarousel";
 import About from "./components/About";
 
 
+
 function App() {
   return (
     <div>
@@ -13,6 +14,7 @@ function App() {
      <MyCarousel/>
      <MyCard/>
      <About/>
+     
     </div>
   );
 }
